ced1f5d092ef245c1fcaf4702cc1e1a6d6459843
AYTSistemas13 <sistemas13@avanceytec.com.mx>
2021-06-09 16:51:18 -0600

    Initiated httpdocs

---
+++ httpdocs/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashop_1',
+    'database_user' => 'prestashop_0',
+    'database_password' => 'VV46bRo#3e',
+    'database_prefix' => 'prstshp_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 's0h5fXUGx8iCIE9bcFYWWh1GVANsvBHrzGUWIjKNESUkf261csjNc3r9HnRjzzIu',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2021-06-09',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'iT8Az3RATE8Q3juv0b3EIMQTWAmvXU4QbB6kX8HP2VgTLwypU3A8ipZRMPXpi6vu',
+    'cookie_iv' => 'sQvDuHevHuMN3niY7xfTXkz2tsBnrGps',
+    'new_cookie_key' => 'def00000cf5811162a3b0a0ea2dc30c502466f1e2183a75ea69a8066d5186513581743464b6127ac4c999b50168f876a947233b54ed5da44113be9c2b2afaf6946f88fdf',
+  ),
+);
